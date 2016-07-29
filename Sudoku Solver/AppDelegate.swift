02d3ac89daeef0b59b2aecb611ae813c78e80347
Matteo Courthoud <Matteo@matteo-macbook-pro.local>
2016-07-29 11:09:16 +0200

    Initial Commit

---
+++ Sudoku Solver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sudoku Solver
+//
+//  Created by Matteo Courthoud on 29/07/16.
+//  Copyright © 2016 Matteo Courthoud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sudoku Solver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sudoku Solver
+//
+//  Created by Matteo Courthoud on 29/07/16.
+//  Copyright © 2016 Matteo Courthoud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
